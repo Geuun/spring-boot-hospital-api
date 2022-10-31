6e3ce6c2e8b9a8f46ec775d53c17f85bf0b20431
KimJaeGeun <jgk9612@gmail.com>
2022-10-31 15:29:14 +0900

    fix: Add exception line \n

--- src/main/java/com/springboothospitalapi/parser/ReadLineContext.java
+++ src/main/java/com/springboothospitalapi/parser/ReadLineContext.java
@@ -25,7 +25,7 @@ public class ReadLineContext<T> {
             try {
                 result.add(parser.parse(str));
             } catch (Exception e) {
-                System.out.printf("파싱중 문제가 생겨 이 라인은 path 합니다. 파일 내용: %s", str);
+                System.out.printf("파싱중 문제가 생겨 이 라인은 path 합니다. 파일 내용: %s \n", str);
             }
         }
         reader.close();
