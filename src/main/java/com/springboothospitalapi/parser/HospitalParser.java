1004a5f3dfd304d0bcf351824777bf780c75d41d
KimJaeGeun <jgk9612@gmail.com>
2022-10-31 20:11:40 +0900

    fix: Preprocess Data & Fix error
    Data가 " 없이 들어오는 문제를 해결
    1. python으로 전처리
    2. bufferedreader가 특정 컬럼을 float으로 읽어오는 문제
    3. FloatParse를 통해 해결
    4. but 다시 캐스팅 작업을 해야함

--- src/main/java/com/springboothospitalapi/parser/HospitalParser.java
+++ src/main/java/com/springboothospitalapi/parser/HospitalParser.java
@@ -3,18 +3,21 @@ package com.springboothospitalapi.parser;
 import com.springboothospitalapi.domain.Hospital;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
 
 public class HospitalParser implements Parser<Hospital>{
     @Override
     public Hospital parse(String str) {
+        // str = "1,의원,01_01_02_P,3620000,PHMA119993620020041100004,19990612,,1,영업/정상,13,영업중,,,,,062-515-2875,,500881.0,광주광역시 북구 풍향동 565번지 4호 3층,광주광역시 북구 동문대로 24 3층 (풍향동),61205.0,효치과의원,20211100000000.0,U,2021.11.17 2:40,치과의원,192630.7351,185314.6176,치과의원,1.0,0.0,0.0,52.29,401,치과,,,,0.0,0.0,,,0.0,"
 
         // String -> int 파싱
-        String[] row = str.split("\",\"");
-//        System.out.println(Arrays.toString(row));
+        String[] row = str.split(",");
+        System.out.println(Arrays.toString(row));
+        System.out.println(row[29]);
 
         Hospital hospital = new Hospital();
 
-        hospital.setId(Integer.parseInt(row[0].replace("\"", "")));
+        hospital.setId(Integer.parseInt(row[0]));
         hospital.setOpenServiceName(row[1]);
         hospital.setOpenLocalGovernmentCode(Integer.parseInt(row[3]));
         hospital.setManagementNumber(row[4]);
@@ -31,9 +34,9 @@ public class HospitalParser implements Parser<Hospital>{
         hospital.setRoadNameAddress(row[19]);
         hospital.setHospitalName(row[21]);
         hospital.setBusinessTypeName(row[25]);
-        hospital.setHealthcareProviderCount(Integer.parseInt(row[29]));
-        hospital.setPatientRoomCount(Integer.parseInt(row[30]));
-        hospital.setTotalNumberOfBeds(Integer.parseInt(row[31]));
+        hospital.setHealthcareProviderCount(Float.parseFloat(row[29]));
+        hospital.setPatientRoomCount(Float.parseFloat(row[30]));
+        hospital.setTotalNumberOfBeds(Float.parseFloat(row[31]));
         hospital.setTotalAreaSize(Float.parseFloat(row[32]));
 
         return hospital;
--- src/test/java/com/springboothospitalapi/parser/HospitalParserTest.java
+++ src/test/java/com/springboothospitalapi/parser/HospitalParserTest.java
@@ -15,7 +15,7 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class HospitalParserTest {
 
-    String line1 = "\"1\",\"의원\",\"01_01_02_P\",\"3620000\",\"PHMA119993620020041100004\",\"19990612\",\"\",\"01\",\"영업/정상\",\"13\",\"영업중\",\"\",\"\",\"\",\"\",\"062-515-2875\",\"\",\"500881\",\"광주광역시 북구 풍향동 565번지 4호 3층\",\"광주광역시 북구 동문대로 24, 3층 (풍향동)\",\"61205\",\"효치과의원\",\"20211115113642\",\"U\",\"2021-11-17 02:40:00.0\",\"치과의원\",\"192630.735112\",\"185314.617632\",\"치과의원\",\"1\",\"0\",\"0\",\"52.29\",\"401\",\"치과\",\"\",\"\",\"\",\"0\",\"0\",\"\",\"\",\"0\",\"\",";
+    String line1 = "1,의원,01_01_02_P,3620000,PHMA119993620020041100004,19990612,,1,영업/정상,13,영업중,,,,,062-515-2875,,500881.0,광주광역시 북구 풍향동 565번지 4호 3층,광주광역시 북구 동문대로 24 3층 (풍향동),61205.0,효치과의원,20211100000000.0,U,2021.11.17 2:40,치과의원,192630.7351,185314.6176,치과의원,1.0,0.0,0.0,52.29,401,치과,,,,0.0,0.0,,,0.0,";
 //    String line770 =
 
     @Autowired
@@ -24,13 +24,17 @@ class HospitalParserTest {
     @Test
     @DisplayName("대용량 데이터에도 잘 파싱이 되는지 Test")
     void oneHundradThousandRowsTest() throws IOException {
-        String fileName = "/Users/geun/Desktop/CodeLion/data/fulldata_01_01_02_P_의원.csv";
+        String fileName = "/Users/geun/Desktop/CodeLion/data/spring-boot-hospital-api/PreprocessedHospitalData.csv";
         List<Hospital> hospitalList = hospitalReadLineContext.readByLine(fileName);
         assertTrue(hospitalList.size() > 1000);
         assertTrue(hospitalList.size() > 10000);
-        for (int i = 0; i < 10; i++) {
+
+        for(int i = 0; i < 10; i++) {
             System.out.println(hospitalList.get(i).getHospitalName());
         }
+
+        System.out.printf("파싱된 데이터의 수: %d", hospitalList.size());
+
     }
 
     @Test
@@ -38,21 +42,22 @@ class HospitalParserTest {
     void convertToHospital() {
 
         HospitalParser hp = new HospitalParser();
+        System.out.println((hp.parse(line1)));
         Hospital hospital = hp.parse(line1);
 
         assertEquals(1, hospital.getId());
         assertEquals("의원", hospital.getOpenServiceName());
-        assertEquals(3620000, hospital.getOpenLocalGovernmentCode());
-        assertEquals("PHMA119993620020041100004", hospital.getManagementNumber());
+        assertEquals(3620000,hospital.getOpenLocalGovernmentCode());
+        assertEquals("PHMA119993620020041100004",hospital.getManagementNumber());
         assertEquals(LocalDateTime.of(1999, 6, 12, 0, 0, 0), hospital.getLicenseDate()); //19990612
         assertEquals(1, hospital.getBusinessStatus());
         assertEquals(13, hospital.getBusinessStatusCode());
         assertEquals("062-515-2875", hospital.getPhone());
         assertEquals("광주광역시 북구 풍향동 565번지 4호 3층", hospital.getFullAddress());
-        assertEquals("광주광역시 북구 동문대로 24, 3층 (풍향동)", hospital.getRoadNameAddress());
+        assertEquals("광주광역시 북구 동문대로 24 3층 (풍향동)", hospital.getRoadNameAddress());
         assertEquals("효치과의원", hospital.getHospitalName());
         assertEquals("치과의원", hospital.getBusinessTypeName());
-        assertEquals(1, hospital.getHealthcareProviderCount());
+        assertEquals(1.0, hospital.getHealthcareProviderCount());
         assertEquals(0, hospital.getPatientRoomCount());
         assertEquals(0, hospital.getTotalNumberOfBeds());
         assertEquals(52.29f, hospital.getTotalAreaSize());
