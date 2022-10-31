0b7629ab76f9610d7be6bb867417df3ac40c1fc1
KimJaeGeun <jgk9612@gmail.com>
2022-10-31 14:48:05 +0900

    Fix: Fix TestCase

--- src/test/java/com/springboothospitalapi/parser/HospitalParserTest.java
+++ src/test/java/com/springboothospitalapi/parser/HospitalParserTest.java
@@ -10,8 +10,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class HospitalParserTest {
 
-    String line1 = "1,의원,01_01_02_P,3620000,PHMA119993620020041100004,19990612,,1,영업/정상,13,영업중,,,,,062-515-2875,,500881,광주광역시 북구 풍향동 565번지 4호 3층,\"광주광역시 북구 동문대로 ";
-    String line770 = 
+    String line1 = "\"1\",\"의원\",\"01_01_02_P\",\"3620000\",\"PHMA119993620020041100004\",\"19990612\",\"\",\"01\",\"영업/정상\",\"13\",\"영업중\",\"\",\"\",\"\",\"\",\"062-515-2875\",\"\",\"500881\",\"광주광역시 북구 풍향동 565번지 4호 3층\",\"광주광역시 북구 동문대로 24, 3층 (풍향동)\",\"61205\",\"효치과의원\",\"20211115113642\",\"U\",\"2021-11-17 02:40:00.0\",\"치과의원\",\"192630.735112\",\"185314.617632\",\"치과의원\",\"1\",\"0\",\"0\",\"52.29\",\"401\",\"치과\",\"\",\"\",\"\",\"0\",\"0\",\"\",\"\",\"0\",\"\",";
+//    String line770 =
 
     @Test
     @DisplayName("csv 1줄을 Hospital로 잘 만드는지 Test")
@@ -25,7 +25,7 @@ class HospitalParserTest {
         assertEquals(3620000,hospital.getOpenLocalGovernmentCode());
         assertEquals("PHMA119993620020041100004",hospital.getManagementNumber());
         assertEquals(LocalDateTime.of(1999, 6, 12, 0, 0, 0), hospital.getLicenseDate()); //19990612
-        assertEquals(6, hospital.getBusinessStatus());
+        assertEquals(1, hospital.getBusinessStatus());
         assertEquals(13, hospital.getBusinessStatusCode());
         assertEquals("062-515-2875", hospital.getPhone());
         assertEquals("광주광역시 북구 풍향동 565번지 4호 3층", hospital.getFullAddress());
