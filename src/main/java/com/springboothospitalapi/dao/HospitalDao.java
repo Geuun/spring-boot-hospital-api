dd33a16ee08ce633fc53cb8384064621cf085e2d
KimJaeGeun <jgk9612@gmail.com>
2022-11-01 16:41:16 +0900

    refactor: Complete RowMapper method
    rowmapper 매서드를 완성

--- src/main/java/com/springboothospitalapi/dao/HospitalDao.java
+++ src/main/java/com/springboothospitalapi/dao/HospitalDao.java
@@ -14,6 +14,26 @@ public class HospitalDao {
         this.jdbcTemplate = jdbcTemplate;
     }
 
+    RowMapper<Hospital> rowMapper = (rs, rowNum) -> {
+        Hospital hospital = new Hospital();
+        hospital.setId(rs.getInt("id"));
+        hospital.setOpenServiceName(rs.getString("open_service_name"));
+        hospital.setOpenLocalGovernmentCode(rs.getInt("open_local_government_code"));
+        hospital.setManagementNumber(rs.getString("management number"));
+        hospital.setLicenseDate(rs.getTimestamp("license_date").toLocalDateTime());
+        hospital.setBusinessStatus(rs.getInt("business_status"));
+        hospital.setBusinessStatusCode(rs.getInt("business_status_code"));
+        hospital.setPhone(rs.getString("phone"));
+        hospital.setFullAddress(rs.getString("full_address"));
+        hospital.setRoadNameAddress(rs.getString("road_name_address"));
+        hospital.setHospitalName(rs.getString("hospital_name"));
+        hospital.setHealthcareProviderCount(rs.getInt("healthcare_provider_count"));
+        hospital.setPatientRoomCount(rs.getInt("patient_room_count"));
+        hospital.setTotalNumberOfBeds(rs.getInt("total_number_of_beds"));
+        hospital.setTotalAreaSize(rs.getFloat("total_area_size"));
+        return hospital;
+    };
+
     public void add(Hospital hospital) {
         String sql = "INSERT INTO `spring-boot-hospital`.`nation_wide_hospitals` (`id`, `open_service_name`, `open_local_government_code`, `management_number`, `license_date`, `business_status`, `business_status_code`, `phone`, `full_address`, `road_name_address`, `hospital_name`, `business_type_name`, `healthcare_provider_count`, `patient_room_count`, `total_number_of_beds`, `total_area_size`)\n" +
                 "VALUES\n" +
@@ -43,16 +63,6 @@ public class HospitalDao {
     public void deleteAll() {
         this.jdbcTemplate.update("DELETE FROM `spring-boot-hospital`.nation_wide_hospitals;");
     }
-
-    RowMapper<Hospital> rowMapper = (rs, rowNum) -> {
-        Hospital hospital = new Hospital();
-
-        hospital.setId(rs.getInt("id"));
-        hospital.setOpenServiceName(rs.getString("open_service_name"));
-        hospital.setHospitalName(rs.getString("hospital_name"));
-
-        return hospital;
-    };
 
     public Hospital findById(int id) {
         return this.jdbcTemplate.queryForObject("SELECT * FROM `spring-boot-hospital`.nation_wide_hospitals WHERE id = ?", rowMapper , id);
