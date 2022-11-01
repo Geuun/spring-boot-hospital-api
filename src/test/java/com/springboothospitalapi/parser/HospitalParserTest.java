7b44f7f78dab545515d6c1a663b65aa49b4272a1
KimJaeGeun <jgk9612@gmail.com>
2022-11-02 00:21:02 +0900

    test: Complete Select test code

--- src/test/java/com/springboothospitalapi/parser/HospitalParserTest.java
+++ src/test/java/com/springboothospitalapi/parser/HospitalParserTest.java
@@ -39,8 +39,19 @@ class HospitalParserTest {
         Hospital selectedHospital = hospitalDao.findById(hospital.getId());
         assertEquals(selectedHospital.getId(), hospital.getId());
         assertEquals(selectedHospital.getOpenServiceName(), hospital.getOpenServiceName());
+        assertEquals(selectedHospital.getOpenLocalGovernmentCode(),hospital.getOpenLocalGovernmentCode());
+        assertEquals(selectedHospital.getManagementNumber(),hospital.getManagementNumber());
+        assertEquals(selectedHospital.getBusinessStatus(), hospital.getBusinessStatus());
+        assertEquals(selectedHospital.getBusinessStatusCode(), hospital.getBusinessStatusCode());
+        assertTrue(selectedHospital.getLicenseDate().isEqual(hospital.getLicenseDate()));
+        assertEquals(selectedHospital.getPhone(), hospital.getPhone());
+        assertEquals(selectedHospital.getFullAddress(), hospital.getFullAddress());
+        assertEquals(selectedHospital.getRoadNameAddress(), hospital.getRoadNameAddress());
         assertEquals(selectedHospital.getHospitalName(), hospital.getHospitalName());
-        assertEquals(selectedHospital.getLicenseDate(), hospital.getLicenseDate());
+        assertEquals(selectedHospital.getBusinessTypeName(), hospital.getBusinessTypeName());
+        assertEquals(selectedHospital.getHealthcareProviderCount(), hospital.getHealthcareProviderCount());
+        assertEquals(selectedHospital.getPatientRoomCount(), hospital.getPatientRoomCount());
+        assertEquals(selectedHospital.getTotalNumberOfBeds(), hospital.getTotalNumberOfBeds());
         assertEquals(selectedHospital.getTotalAreaSize(), hospital.getTotalAreaSize());
     }
 
