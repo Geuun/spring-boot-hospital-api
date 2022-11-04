502c7dc7e53d2169ae94b7df7b6c59734f3b0792
KimJaeGeun <jgk9612@gmail.com>
2022-11-05 02:54:57 +0900

    feat: Add Slf4j to check logs

--- src/main/java/com/springboothospitalapi/controller/HospitalController.java
+++ src/main/java/com/springboothospitalapi/controller/HospitalController.java
@@ -3,6 +3,7 @@ package com.springboothospitalapi.controller;
 import com.springboothospitalapi.dao.HospitalDao;
 import com.springboothospitalapi.domain.Hospital;
 import com.springboothospitalapi.domain.dto.HospitalDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +13,7 @@ import java.util.Optional;
 /**
  * Todo: id 110000의 1병원 이름, 2주소, 3도로명주소, 4의료진 수, 5병상 수, 6면적, 7폐업여부
  */
+@Slf4j
 @RestController
 @RequestMapping("/hospitals")
 public class HospitalController {
@@ -22,8 +24,9 @@ public class HospitalController {
         this.hospitalDao = hospitalDao;
     }
 
-    @GetMapping("/select/{id}")
+    @GetMapping("/get/{id}")
     public ResponseEntity<Hospital> get(@PathVariable Integer id) {
+        log.info("/get/{id}, findById()");
         Hospital hospital = hospitalDao.findById(id);
         Optional<Hospital> opt = Optional.of(hospital);
 
@@ -36,12 +39,14 @@ public class HospitalController {
 
     @DeleteMapping("/delete/{id}")
     public String deleteById(@PathVariable Integer id) {
+        log.info("/delete/{id}, deleteById");
         hospitalDao.deleteById(id);
         return id + "에 해당하는 정보가 삭제되었습니다.";
     }
 
-    @DeleteMapping("/deleteall")
+    @DeleteMapping("/delete/deleteall")
     public String deleteAll() {
+        log.info("/delete/deleteall, deleteAll()");
         hospitalDao.deleteAll();
         return "모든 데이터가 삭제되었습니다.";
     }
